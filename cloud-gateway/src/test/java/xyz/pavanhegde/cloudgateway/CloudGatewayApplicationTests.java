a332705297eac75a388ad21bfdd14f072b47971a
Pavan Hegde <pavanrkadave@gmail.com>
2024-11-19 23:07:18 +0100

    Added cloud gateway

---
+++ cloud-gateway/src/test/java/xyz/pavanhegde/cloudgateway/CloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.pavanhegde.cloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
